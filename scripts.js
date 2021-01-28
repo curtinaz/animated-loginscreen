8dfd6abed4ff7284435fb50d56bcce66dbc76b73
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-01-27 22:50:48 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ const isItMail = () => {
     if (email.value!="") {
         console.log("email")
     } else {
-        email.style="border: 0.5px #b9b9b9 solid;"
+        email.style="border: 0.5px #c51212 solid;"
     }
 }
 
