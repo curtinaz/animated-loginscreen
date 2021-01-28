0ff64305f3bcda55bc2c25f7886d0914bb050b6e
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-01-27 22:41:46 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 email = document.querySelector("#emailInput").value;
 
-const isItMail = () => console.log("change")
+const isItMail = () => console.log("changed")
 
 email.addEventListener("change", isItMail())
 
