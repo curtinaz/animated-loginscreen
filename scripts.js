eb09c0962c694911ae4c9483420a17400af0de6d
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-01-30 13:32:30 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ email = document.querySelector("#emailInput");
 
 const isItMail = () => {
     if (email.value!="") {
-        email.style=""
+        email.style="";
     } else {
         email.style="border: 2px #ff0000 solid;"
         email.placeholder="O e-mail não pode ficar em branco!";
