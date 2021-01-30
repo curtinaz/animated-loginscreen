4250acf1639606804f6cf4987cc9bacee62aae7a
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-01-30 13:42:41 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,7 +5,10 @@ const isItMail = () => {
         email.style="";
         email.placeholder="E-mail *";
 
-        if(email.value.search(@))
+        if(email.value.search("@")==-1) {
+            email.style="border: 2px #ff0000 solid;"
+            email.placeholder="Email invalido";    
+        }
 
     } else {
         email.style="border: 2px #ff0000 solid;"
