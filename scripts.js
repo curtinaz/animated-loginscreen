e567a0780f99f37d8168d283b1a9fb571233f2cd
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-01-30 13:35:23 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
 email = document.querySelector("#emailInput");
 
-const isItMail = () => {
+const isItMail = (mail) => {
     if (email.value!="") {
         email.style="";
         email.placeholder="E-mail *";
+
+        var exclude=/[^@-.w]|^[_@.-]|[._-]{2}|[@.]{2}|(@)[^@]*1/;
+        var check=/@[w-]+./;
+        var checkend=/.[a-zA-Z]{2,3}$/;
+        if(((mail.search(exclude) != -1)||(mail.search(check)) == -1)||(email.search(checkend) == -1)){return false;}
+        else {return true;}
+
     } else {
         email.style="border: 2px #ff0000 solid;"
         email.placeholder="O e-mail não pode ficar em branco!";
